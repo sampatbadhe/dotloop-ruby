ba72bc562a2ced991f8182c2a6a468f73f3515c8
Aravind Vemula <aravind@kiprosh.com>
2019-06-26 21:53:53 +0530

    upgrade: gem plissken (#6)

--- dotloop-ruby.gemspec
+++ dotloop-ruby.gemspec
@@ -32,7 +32,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency 'httparty', '~> 0.13'
   spec.add_runtime_dependency 'virtus', '~> 1.0'
-  spec.add_runtime_dependency 'plissken', '~> 0.2'
+  spec.add_runtime_dependency 'plissken', '~> 1.2'
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- lib/dotloop/client.rb
+++ lib/dotloop/client.rb
@@ -116,13 +116,23 @@ module Dotloop
 
     def self.snakify(hash)
       if hash.is_a? Array
-        hash.map(&:to_snake_keys)
+        hash.map{ |item| symbolize(item.to_snake_keys) }
       else
-        hash.to_snake_keys
+        symbolize(hash.to_snake_keys)
       end
     end
 
     private
+
+    def self.symbolize(obj)
+      return obj.reduce({}) do |memo, (k, v)|
+        memo.tap { |m| m[k.to_sym] = symbolize(v) }
+      end if obj.is_a? Hash
+      return obj.reduce([]) do |memo, v|
+        memo << symbolize(v); memo
+      end if obj.is_a? Array
+      obj
+    end
 
     def download_headers
       {
